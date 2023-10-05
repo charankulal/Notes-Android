4b4955c77b1d5315736d882093dcc3aa2200778f
Charan <charankulal0241@gmail.com>
2023-10-05 19:43:16 +0530

    Added logout from note app feature

--- app/src/main/java/com/example/notes/MainActivity.java
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -7,10 +7,13 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.ImageButton;
+import android.widget.PopupMenu;
 
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.Query;
 
 public class MainActivity extends AppCompatActivity {
@@ -45,7 +48,24 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void showMenu() {
-        //TODO: display menu
+        PopupMenu popupMenu=new PopupMenu(MainActivity.this,menuButton);
+        popupMenu.getMenu().add("Logout");
+        popupMenu.show();
+
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if(item.getTitle()=="Logout")
+                {
+                    FirebaseAuth.getInstance().signOut();
+                    startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                    finish();
+                    return true;
+                }
+                return false;
+            }
+        });
+
     }
 
     @Override
